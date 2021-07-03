6e448946dc45033713602fc1ffd3017bc8a0ccb5
somukhannan <somukhannan8@gmail.com>
2021-07-03 08:10:35 +0000

    Installized

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { React } from 'react';
+import './App.css';
+import { Button } from '@material-ui/core';
+
+const App = () =>
+	<Button color="primary">Hello World</Button>
+	;
+
+export default App;
