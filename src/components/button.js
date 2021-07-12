0e0f1b9f8963ded3cdab8da9b96b11d749b2b625
somukhannan <somukhannan8@gmail.com>
2021-07-12 14:51:37 +0000

    variant Buttons

--- src/components/button.js
+++ src/components/button.js
@@ -2,11 +2,22 @@ import { React } from 'react';
 import { Button } from '@material-ui/core';
 import context from '../core/context';
 
-const simpleButton = () =>{
-    return (
-        <Button style={ { fontSize: `${ context.state.fontSize }px` } } color="primary">Hello World</Button>
-        <Button variant="contained">Default</Button>
-    );
-};
+const simpleButton = () =>
+	<div>
+		<Button
+			style={ { fontSize: `${ context.state.fontSize }px` } }
+			color="primary"
+		>Hello World
+		</Button>
+		<Button variant="outlined" color="secondary">
+			Secondary
+		</Button>
+		<Button variant="contained" color="inherit">
+			Third</Button>
+		<Button variant="text" color="default">
+			Fourth</Button>
+		<Button variant="text" color="default">
+			Fifth</Button>
+	</div>;
 
 export default simpleButton;
