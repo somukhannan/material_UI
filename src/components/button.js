c7bf617dabc7ee81d8d0a7d002e2babceacd4de3
somukhannan <somukhannan8@gmail.com>
2021-07-12 14:11:37 +0000

    button refractored

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,11 @@ import { React } from 'react';
 import { Button } from '@material-ui/core';
 import context from '../core/context';
 
-const simpleButton = () => <Button style={ { fontSize: `${ context.state.fontSize }px` } } color="primary">Hello World </Button>;
+const simpleButton = () =>{
+    return (
+        <Button style={ { fontSize: `${ context.state.fontSize }px` } } color="primary">Hello World</Button>
+        <Button variant="contained">Default</Button>
+    );
+};
 
 export default simpleButton;
