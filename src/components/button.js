62bb622c139b55625d6292ffee4f1d421ab5ba53
somukhannan <somukhannan8@gmail.com>
2021-07-13 14:15:08 +0000

    Button href added

--- src/components/button.js
+++ src/components/button.js
@@ -7,17 +7,15 @@ const simpleButton = () =>
 		<Button
 			style={ { fontSize: `${ context.state.fontSize }px` } }
 			color="primary"
-		>Hello World
-		</Button>
-		<Button variant="outlined" color="secondary">
-			Secondary
-		</Button>
-		<Button variant="contained" color="inherit">
-			Third</Button>
-		<Button variant="text" color="default">
-			Fourth</Button>
-		<Button variant="text" color="default">
-			Fifth</Button>
+		>Hello World</Button>
+		<Button variant="outlined" color="secondary">Secondary</Button>
+		<Button variant="contained" color="inherit">Third</Button>
+		<Button variant="text" color="default">Fourth</Button>
+		<Button variant="text" disabled="disabled">Fifth-disabled</Button>
+		<Button variant="text" color="secondary" href="https://www.google.com">
+			Sixth-href</Button>
+		<Button variant="text" color="secondary" href="#contained-buttons">
+			Seventh-href</Button>
 	</div>;
 
 export default simpleButton;
